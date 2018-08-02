e60d814fed345467af8605b7a0b9ceeb83142b6d
jrrod507 <jrodriguez18@gmail.com>
2018-08-01 20:19:08 -0400

    Created store and displayed a couple properties.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>Sweetwater</h1>
+    <h6>Products</h6>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li v-for="product in products">
+        <div class="product-container">
+          <span>{{ product.productName }}</span>
+          <hr>
+          <span>{{ product.price }}</span>
+        </div>
+      </li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+
+  computed: {
+    products() {
+      return this.$store.state.products;
+    }
+  },
+  
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+     
     }
   }
 }
@@ -38,6 +40,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.product-container {
+  border: 1px solid black;
 }
 
 h1, h2 {
